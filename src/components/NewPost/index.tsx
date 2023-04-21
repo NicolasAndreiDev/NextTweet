0f264ea3e88601b428815aa697667f7520c98293
NicolasAndreiDev <nicolasandreislc@gmail.com>
2023-04-21 00:50:04 -0300

    RotaPost

--- src/components/NewPost/index.tsx
+++ src/components/NewPost/index.tsx
@@ -2,24 +2,40 @@ import { db, auth } from '../../../firebase'
 import { setDoc, doc, getDoc } from "firebase/firestore";
 import { UserContext } from "@/providers/userProvider";
 import { useContext } from 'react'
+import { format } from 'date-fns';
 
 export default function NewPost({children, imagem, text, className}: {children: React.ReactNode, className: string, imagem: string, text: string}) {
     const { user } = useContext(UserContext)
 
     async function handlePost(username: string, perfilImageUrl: string) {
+        const currentDate = new Date();
+        const like: number | null = null
         const currentUser = auth.currentUser;
-        if (currentUser) {
-          const userDocRef = doc(db, 'users', currentUser.uid);
-          const userDoc = await getDoc(userDocRef);
-          const userData = userDoc.data();
-          const posts = userData?.posts ? [...userData.posts, { imagem, text, username, perfilImageUrl }] : [{ imagem, text, username, perfilImageUrl}];
-          await setDoc(userDocRef, { posts: posts }, { merge: true });
+        if (!currentUser) return;
+        const userDocRef = doc(db, 'users', currentUser.uid);
+        const userDoc = await getDoc(userDocRef);
+        const userData = userDoc.data();
+        let nextIndex = 0;
+        if (userData?.posts) {
+            nextIndex = userData.posts.length;
         }
-      }
-    
-    return(
-        <>
-            <button className={className} onClick={() => handlePost(user!.username, user!.perfilImageUrl)}> {children} </button>
-        </>
-    )
+        const formattedDate = format(currentDate, 'MMM d');
+        const post = {
+            imagem,
+            text,
+            username,
+            perfilImageUrl,
+            id: `${username}-${nextIndex}`,
+            date: formattedDate,
+            likes: like
+        };
+        const posts = userData?.posts ? [...userData.posts, post] : [post];
+        await setDoc(userDocRef, { posts }, { merge: true });
+    }
+
+    return (
+        <button className={className} onClick={() => handlePost(user?.username || '', user?.perfilImageUrl || '')}>
+            {children}
+        </button>
+    );
 }
