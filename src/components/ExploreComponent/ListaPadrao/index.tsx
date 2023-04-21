2c9eb0acad53a67c3ac8d344591a11b991a91c62
NicolasAndreiDev <nicolasandreislc@gmail.com>
2023-04-21 12:59:48 -0300

    .

--- src/components/ExploreComponent/ListaPadrao/index.tsx
+++ src/components/ExploreComponent/ListaPadrao/index.tsx
@@ -5,9 +5,9 @@ export default function ListaPadrao({lista}: {lista: Array<{type:string ,title: 
 
     return(
         <>
-            {lista.map(item => {
+            {lista.map((item, index) => {
             return(
-                <div className={styles.lista}>
+                <div className={styles.lista} key={index}>
                     <span className={styles.type}>{item.type}</span>
                     <span className={styles.title}>{item.title}</span>
                     <span className={styles.posts}>{item.posts}</span>
