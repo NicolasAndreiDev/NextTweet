9f13b9d300d93812298b01cb2342bdcd17443f37
NicolasAndreiDev <nicolasandreislc@gmail.com>
2023-04-24 21:09:54 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,23 +1,18 @@
-/** @type {import('next').NextConfig} */
-const withEnv = require('next-env');
+/** @type {import('next-env')} */
 const dotenvLoad = require('dotenv-load');
-const webpack = require('webpack');
 
 dotenvLoad();
 
-module.exports = withEnv({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
-      new webpack.ProvidePlugin({
-        fs: 'fs',
-      })
-    );
+module.exports = {
+  future: {
+    webpack5: true, 
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false, 
+    };
+
     return config;
   },
-});
+};
